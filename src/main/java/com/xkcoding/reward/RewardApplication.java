32f30343fa50753205bc0fdebc1635643106e73b
Yangkai.Shen <237497819@qq.com>
2019-01-26 09:26:26 +0800

    :heavy_plus_sign: 添加 hutool 依赖

--- src/main/java/com/xkcoding/reward/RewardApplication.java
+++ src/main/java/com/xkcoding/reward/RewardApplication.java
@@ -3,10 +3,23 @@ package com.xkcoding.reward;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * <p>
+ * 启动器
+ * </p>
+ *
+ * @package: com.xkcoding.reward
+ * @description: 启动器
+ * @author: yangkai.shen
+ * @date: Created in 2019-01-27 23:21
+ * @copyright: Copyright (c) 2019
+ * @version: V1.0
+ * @modified: yangkai.shen
+ */
 @SpringBootApplication
 public class RewardApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RewardApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RewardApplication.class, args);
+    }
 }
